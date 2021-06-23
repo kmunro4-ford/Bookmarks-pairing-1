06b00d84af17a9922d8df94b64cf74075eb59055
Kelvin Munro <kmunro4@ford.com>
2021-06-23 16:16:07 +0100

    refactored the refactor to have it more refactored

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,26 +1,23 @@
 class Bookmark
 
   def self.all
-    # bookmark_array = ["www.google.com", "www.bing.com", "www.ign.com"]
-    if ENV['ENVIRONMENT'] == 'test'
-      conn = PG::Connection.open(:dbname => 'bookmark_manager_test')
-      res = conn.exec_params('SELECT url FROM bookmarks')
-      return res.values.join(", ")
-    else
-      conn = PG::Connection.open(:dbname => 'bookmark_manager')
-      res = conn.exec_params('SELECT url FROM bookmarks')
-      return res.values.join(", ")
-    end
+    self.setup
+    res = @conn.exec_params('SELECT url FROM bookmarks')
+    return res.values.join(", ")
   end
 
   def self.add_to_table(bookmark)
-    if ENV['ENVIRONMENT'] == 'test'
-      conn = PG::Connection.open(:dbname => 'bookmark_manager_test')
-      conn.exec("INSERT INTO bookmarks (url) VALUES ('#{bookmark}');")
-    else
-      conn = PG::Connection.open(:dbname => 'bookmark_manager')
-      conn.exec("INSERT INTO bookmarks (url) VALUES ('#{bookmark}');")
-    end
+    self.setup
+    @conn.exec("INSERT INTO bookmarks (url) VALUES ('#{bookmark}');")
   end
 
+  private
+  def self.setup
+    if ENV['ENVIRONMENT'] == 'test'
+      @conn = PG::Connection.open(:dbname => 'bookmark_manager_test')
+    else
+      @conn = PG::Connection.open(:dbname => 'bookmark_manager')
+    end
+  end
+  
 end
